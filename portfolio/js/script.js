ac37589f7eca83d6fb0a070cf5df444a27fdd27f
David <dc82927n@gmail.com>
2019-10-05 13:23:53 -0400

    add some options features to the superslider document ready.

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -2,5 +2,9 @@
 
 /* saying when the document is ready do this and a webpage is a document. */
 $(document).ready(function() {
-    $('#slides').superslides()
+    $('#slides').superslides( {
+        animation: "fade", 
+        play: 2000 // in mill seconds
+        
+    });
 });
